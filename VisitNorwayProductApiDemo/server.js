75755401adb0ad7786dfc2d634396d001ab49398
unknown <joachim.lovf@osl-jolo02.co.int>
2014-04-12 19:08:44 +0200

    init

---
+++ VisitNorwayProductApiDemo/server.js
@@ -0,0 +1,36 @@
+﻿var express = require( 'express' );
+var http = require( 'http' );
+var app = express();
+
+app.get( '/products', function ( req, res ) {
+    var distance = req.params.dist;
+    var latitude = req.params.latitude;
+    var longitude = req.params.longitude;
+    var limit = req.params.limit;
+    if ( !limit ) {
+        limit = 100;
+    }
+    var url = "http://product.test.visitnorway.com/api/products?sort=distance&latitude=" + latitude; //+ "&longitude=" + longitude + "&withinDistance=" + distance + "&limit=" & limit;
+    console.log( "Getting " + url );
+    http.get( url, function ( response ) {
+        // data is streamed in chunks from the server
+        // so we have to handle the "data" event    
+        var buffer = "",
+            data,
+            route;
+
+        response.on( "data", function ( chunk ) {
+            buffer += chunk;
+        });
+
+        response.on( "end", function ( err ) {
+            res.send( buffer );
+        });
+    });
+});
+
+var server = app.listen( 3000, function () {
+    console.log( 'Listening on port %d', server.address().port );
+});
+
+
